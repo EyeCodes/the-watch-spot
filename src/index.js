a1eb5841f43597d8aba9b431cedeb53baf2acbba
Yamski1 <georgewilliammorano@gmail.com>
2024-07-13 20:39:30 +0800

    update

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 document.getElementById('buy-now-btn').onclick = function() {
     document.getElementById('modal').style.display = 'block';
+    document.body.classList.add('no-scroll');
 }
 
 document.getElementsByClassName('close-btn')[0].onclick = function() {
     document.getElementById('modal').style.display = 'none';
+    document.body.classList.remove('no-scroll');
 }
+
